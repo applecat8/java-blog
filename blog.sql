d47a62c2b0956fb4a01c434899afc4de149d11a7
applecat8 <3075202650@qq.com>
2021-03-24 22:21:59 +0800

    标签页后台管理

--- blog.sql
+++ blog.sql
@@ -1,1 +1,12 @@
 create database blog
+desc t_blog
+desc t_type
+select * from t_user
+select * from t_blog
+select * from t_type
+select * from t_tag
+insert into t_user values(null,1,"",now(),"123456@qq.com","ib","a",now(),"a")
+insert into t_type values(null,"Golang")
+alter table t_user drop column nick_name
+drop database blog
+create database blog
--- blog/blog/src/main/java/com/applecat/blog/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.applecat.blog.service;
-
-import com.applecat.blog.model.pojo.User;
-
-public interface UserService {
-    public boolean isAdmin(String name,String password);
-
-    public User checkUser(String name,String password);
-}
